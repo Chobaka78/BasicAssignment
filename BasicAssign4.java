f00a79d2b286eed743f5351e4b72ae36e81681c6
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2018-09-25 17:28:51 -0400

    Update 4
    Question 4 done

--- BasicAssign4.java
+++ BasicAssign4.java
@@ -1,8 +1,26 @@
 //BasicAssign4.java
+//Usman Farooqi
+//
 import java.util.*;
 class BasicAssign4{
 	public static void main(String [] args){
 		int number [] = new int[365];
-		
+		Random x = new Random();
+		//int x = (int)(Math.random()*365) + 1;
+		int counter = 0;
+		for (int i = 0; i< 10000; i++){
+			number = new int[365];
+			while (true){
+				int w= x.nextInt(365);
+				counter += 1;
+				if (number[w] == 0){
+					number[w] += 1;
+				}
+				else{
+					break;
+				}
+			}
+		}
+		System.out.print(counter/10000);
 	}
 }
